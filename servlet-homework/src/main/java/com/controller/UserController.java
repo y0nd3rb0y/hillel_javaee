56fb42602267f19e6eee60ead073e0dbf14849a2
Mykhaylo Samonov <y0nd3rb0y@gmail.com>
2016-12-10 22:39:32 +0200

    Servlet w/ POST auth homework

--- servlet-homework/src/main/java/com/controller/UserController.java
+++ servlet-homework/src/main/java/com/controller/UserController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,35 +33,36 @@ public class UserController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        if (username != null
-                && username.length() > 0
-                && password != null
-                && password.length() > 0
-                && userService.isUserAuthenticated(username, password)) {
-            response.getWriter().println("<h2>Hello, " + username + "!</h2>");
-        } else {
+
             response.getWriter().println("<html>"
                     + "<head><title>Servlet Authentication</title></head>"
                     + "<body>"
-                    + "<form method=\"get\">"
+                    + "<form method=\"post\">"
                     + "<input type=\"text\" name=\"username\" size=\"25\">"
                     + "<input type=\"password\" name=\"password\" size=\"25\">"
                     + "<p></p>"
                     + "<input type=\"submit\" value=\"Submit\">"
                     + "</form>"
                     + "</body></html>");
-            if(username != null){
-                response.getWriter().println("<h2>Authentication failed!</h2>");
-            }
+
         }
 
-
-    }
-
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        PrintWriter responseWriter = response.getWriter();
+        responseWriter.println("<html><body>");
+        if (username != null
+                && username.length() > 0
+                && password != null
+                && password.length() > 0
+                && userService.isUserAuthenticated(username, password)) {
+            responseWriter.println("<h2>Hello, " + username + "!</h2>");
+        } else {
+                responseWriter.println("<h2>Authentication failed!</h2>");
+            }
+        responseWriter.println("</body></html");
+
     }
 }
